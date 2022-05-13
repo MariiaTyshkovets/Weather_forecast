fc55f1d95d5bcce24b510a21efb9521bffa1deea
Mariia Tyshkovets <maria15021996@gmail.com>
2022-05-13 13:57:52 +0300

    the task is done

--- script/index.js
+++ script/index.js
@@ -1,23 +1,65 @@
+const container = document.querySelector('.container');
 const block = document.querySelectorAll('.block');
+const addInfo = document.querySelector('.add-info')
 const API =  'https://api.openweathermap.org/data/2.5/weather?q='
 const APIkey = 'a89fb7179c9a1d022384d57c5a0a7dff';
 
 for (let i = 0; i < block.length; i++) {
-    block[i].addEventListener('click', function () {
-        const collection = this.children;
-        const town = collection[0].innerHTML; 
-        fetch(`${API + town}&units=metric&APPID=${APIkey}`)   
-        .then(response => response.json())
-        .then(cityinfo => {
-            let degree = collection[2];
-            degree.innerHTML = `${Math.round(cityinfo.main.temp)} &#8451`;
-            const img = collection[3];
-            let weatherIcon = cityinfo.weather[0].icon;
-            img.src = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-            console.log(cityinfo)
-        })
-        .catch(error => console.log(error));
+    const collection = block[i].children;
+    const town = collection[0].innerHTML; 
+    fetch(`${API + town}&units=metric&APPID=${APIkey}`)   
+    .then(response => response.json())
+    .then(cityinfo => {
+        let degree = collection[2];
+        degree.innerHTML = `${Math.round(cityinfo.main.temp)} &#8451`;
+        const img = collection[3];
+        let weatherIcon = cityinfo.weather[0].icon;
+        img.src = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     })
+    .catch((error) => {
+        degree.innerHTML = `The service is not available`;
+        return console.log(error)
+    });
 }
+
+container.addEventListener('click', function (event) {
+    const town = event.path.sort()[4].querySelector('.town').innerHTML;
+    const infoPlus = document.querySelector('.info');
+    infoPlus.innerHTML = '';
+    fetch(`${API + town}&units=metric&APPID=${APIkey}`)   
+    .then(response => response.json())
+    .then(info => {
+        const tempMin = Math.round(info.main.temp_min);
+        const tempMax = Math.round(info.main.temp_max);
+        const humidity = info.main.humidity;
+        const weather = info.weather[0].description;
+        const windSpeed = info.wind.speed;
+
+        const h2 = document.createElement('H2');
+        const p = document.createElement('P');
+
+        h2.innerHTML = `Additional weather information in ${town}`;
+        p.innerHTML = `Minimum temperature: ${tempMin} &#8451.<br>Maximum temperature: ${tempMax} &#8451.<br>Wind speed: ${windSpeed} m/s.<br>Relative humidity: ${humidity}%.<br>Description of weather conditions: ${weather}.`;
+
+        infoPlus.appendChild(h2);
+        infoPlus.appendChild(p);
+    })
+    .catch((error) => {
+        degree.innerHTML = `The service is not available`;
+        return console.log(error);
+    });
+    container.classList.add('blur');
+    addInfo.classList.remove('none');    
+});
+
+const displayNone = () => {
+    container.classList.remove('blur');
+    addInfo.classList.add('none');
+} 
+
+addInfo.addEventListener('click', displayNone);
+document.querySelector('.close').addEventListener('click', displayNone);
+
+
 
 
